00a185a6cc39d71798009cc4f02554bacc69af10
Jin-youngdo <77922432+Jin-youngdo@users.noreply.github.com>
2021-07-03 16:14:43 +0900

    clear Btn + randomColor

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 // jssave 아이디 불러오기 : save 버튼
 const saveBtn = document.getElementById("jsSave");
+// jsClear 아이디 불러오기 : clear 버튼
+const clearBtn = document.getElementById("jsClear");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
 
 let painting = false;
 let filling = false;
+let random_color = false;
 
 // canvas pixel modifier 지정
 canvas.width = CANVAS_SIZE;
@@ -27,16 +30,14 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 // canvas stroke style : 색 지정
 ctx.strokeStyle = INITIAL_COLOR;
 // canvas fill style 초기화 
-ctx.fillstyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 // canvas line 두께 지정
 ctx.lineWidth = 2.5;
 
 
 // painting 변수 조정 함수
 function startPainting(){
-    if(filling === false){
-        painting = true;
-    }
+    painting = true;
 }
 
 // 마우스가 클릭이 아닐 시 event : 종료
@@ -44,10 +45,20 @@ function stopPainting(){
     painting = false;
 }
 
+function onMouseDown(event) {
+    if(random_color){
+        color = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+        ctx.strokeStyle = color;
+        ctx.fillStyle = color;
+    }
+
+    startPainting();
+}
+
 // 마우스 이동시 event 
 function onMouseMove(event){
-    const x = event.offsetX;
-    const y = event.offsetY;
+    let x = event.offsetX;
+    let y = event.offsetY;
     if(!painting){
         // 마우스 active가 아닌 hover 상태일 시  현재 (x, y) 좌표에 path(선) 생성
         ctx.beginPath();
@@ -61,8 +72,13 @@ function onMouseMove(event){
 
 // color 버튼 클릭시 색상 값 변경 함수
 function handleColorClick(event){
-    const color = event.target.style.backgroundColor;
-    // strokeStyle override
+    if(!event.target.classList.contains("random_color")){
+        color = event.target.style.backgroundColor;
+
+    } else {
+        random_color = true;
+        color = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`; 
+    }
     ctx.strokeStyle = color;
     ctx.fillStyle = color;
 }
@@ -98,7 +114,7 @@ function handleCM(event){
     event.preventDefault();
 }
 
-// canvas SAVE 버튼 활성화
+// canvas SAVE 버튼 클릭시 event
 function handleSaveClick(){
     const image = canvas.toDataURL();
     const link = document.createElement("a");
@@ -107,9 +123,14 @@ function handleSaveClick(){
     link.click();
 }
 
+// canvas Clear 버튼 클릭시 event
+function handleClearClick(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mousedown", onMouseDown);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", handleCanvasClick);
@@ -130,4 +151,8 @@ if(mode){
 
 if(saveBtn){
     saveBtn.addEventListener("click", handleSaveClick);
+}
+
+if(clearBtn){
+    clearBtn.addEventListener("click", handleClearClick);
 }
